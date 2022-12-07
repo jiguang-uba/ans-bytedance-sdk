3ca8693f4e209161ac51d50d8b27a7d466614b29
huangriya <huangriya@163.com>
2022-12-07 15:17:37 +0800

    sdk version 4.3.1

---
+++ README.md
@@ -0,0 +1,10 @@
+# Bytedance SDK
+
+========
+
+This is the official Bytedance SDK.
+
+# License
+
+[gpl-3.0](https://www.gnu.org/licenses/gpl-3.0.txt)
+
---
+++ build/encryption/rollup.config.encryption.js
@@ -0,0 +1,66 @@
+import babel from 'rollup-plugin-babel'
+import eslint from 'rollup-plugin-eslint'
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import { uglify } from 'rollup-plugin-uglify'
+
+export default [{
+    input: './src/configure/base/encrypt.js',
+    output: [
+        {
+            file: './Demo/util/sdk/AnalysysAgent_encryption.min.js',
+            format: 'cjs',
+            name: 'Ans',
+            plugins: [uglify({
+                'mangle': {
+                    toplevel: true
+                }
+            })]
+        }, {
+            file: './taroDemo/src/sdk/AnalysysAgent_encryption.min.js',
+            format: 'cjs',
+            name: 'Ans',
+            plugins: [uglify({
+                'mangle': {
+                    toplevel: true
+                }
+            })]
+        }, {
+            file: './SDK/AnalysysAgent_encryption.min.js',
+            format: 'cjs',
+            name: 'Ans',
+            plugins: [uglify({
+                'mangle': {
+                    toplevel: true
+                }
+            })]
+        }, {
+            file: './SDK/AnalysysAgent_encryption.es6.min.js',
+            format: 'esm',
+            name: 'Ans',
+            plugins: [
+                terser({
+                    mangle: {
+                        toplevel: true
+                    }
+                })
+            ]
+        }
+    ],
+    plugins: [
+        resolve({
+            jsnext: true,
+            main: true,
+            browser: true,
+        }),
+        commonjs(),
+        eslint({
+            exclude: [
+                'src/**'
+            ]
+        }),
+        babel({
+            exclude: 'node_modules/**'
+        })
+    ]
+}]
